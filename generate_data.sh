3a1a433825c5d8a9741e4f8ef627277424b170d1
Dennis Asamoah Owusu <dennisasamoahowusu@gmail.com>
2018-03-13 20:33:10 -0400

    update

--- generate_data.sh
+++ generate_data.sh
@@ -10,20 +10,28 @@ COALESCE_FILE_SIZE="5000000"
 #how many rounds of random chunkcs
 RANDOM_CHUNKS_ROUNDS=5
 
+#chunk size in kilobytes
+CHUNK_SIZE=50
+
 WIKIPEDIA_HAR="winter-war-en.wikipedia.org.har"
 WIKIPEDIA_HAR_DOCS="winter-war-en.wikipedia.org-docs"
-WIKIPEDIA_COALESCE="coalesce-output"
+WIKIPEDIA_COALESCE="wikipedia-coalesce-output"
+WIKIPEDIA_RANDOM_CHUNKS="wikipedia-random-chunks"
 
+rm -rf $WIKIPEDIA_RANDOM_CHUNKS
+mkdir $WIKIPEDIA_RANDOM_CHUNKS
 
 python2.7 ../datacomp_utils/har2docs.py --stream $WIKIPEDIA_HAR $WIKIPEDIA_HAR_DOCS  wikipedia
 python coalesce.py $WIKIPEDIA_HAR_DOCS $WIKIPEDIA_COALESCE $COALESCE_FILE_SIZE
 
+COUNTER=1
 FILES=$WIKIPEDIA_COALESCE/*
 for f in $FILES
 do
-  for i in `seq 1 $RANDOM_CHUNKS_ROUNDS`;
+  for k in `seq 1 $RANDOM_CHUNKS_ROUNDS`;
     do
-      echo $f
+      ../randomchunk/randomchunk -i $f -o $WIKIPEDIA_RANDOM_CHUNKS/$COUNTER -s $CHUNK_SIZE
+      let COUNTER=COUNTER+1
     done 
 done
 
